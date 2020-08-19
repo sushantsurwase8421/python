7aa97e1c22182a0cd2aebbc42765785322ad8c68
sushantsurwase8421 <62616518+sushantsurwase8421@users.noreply.github.com>
2020-08-19 19:08:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import smtplib
 
 ip = "localhost"
 port = 80
-emails = ["surwase.sushant.d@gmail.com","bhushan.gaikwad@outlook.com"] 
+emails = ["uname@gmail.com","uname@gmail.com"] 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 result = sock.connect_ex((ip, port))
@@ -18,7 +18,7 @@ else:
    for dest in emails: 
        s = smtplib.SMTP('smtp.gmail.com', 587) 
        s.starttls() 
-       s.login("surwase.sushant.d@gmail.com", "Developer@2020") 
+       s.login("uname@gmail.com", "email_pwd") 
        SUBJECT = "POD DOWN"   
        TEXT = "Trigger new one"
        message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
